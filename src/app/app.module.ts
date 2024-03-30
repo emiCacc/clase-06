def9ea71e9ba8f45213890e4a1686cb65cf8f527
emiliano.morales <emiliano.morales@bizitglobal.com>
2024-03-30 19:29:31 -0300

    limpiando codigo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ReactiveFormsComponent } from './components/reactive-forms/reactive-forms.component';
 import { InscripcionComponent } from './components/inscripcion/inscripcion.component';
 import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReactiveFormsComponent,
     InscripcionComponent,
     HeaderComponent
   ],
